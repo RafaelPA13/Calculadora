c44d3f2f1b46c2f545ad1c3a10859c27ed70dc0c
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-16 20:39:04 -0300

    HTML e JS completos

--- src/script.js
+++ src/script.js
@@ -1,13 +1,42 @@
+// Imprime o resultado da operação na tela
 let resultado = document.getElementById('resultado')
 
-function calcular() {
-  // Variaveis de entrada
+function soma() {
+  // Números
   let n1 = parseFloat(document.getElementById("numero1").value);
   let n2 = parseFloat(document.getElementById("numero2").value);
-
-  // Processamento
+  // Operação
   let soma = n1 + n2;
-
-  // Saída
-  resultado.textContent = `O resultado é ${soma}`
+  // Resultado
+  resultado.textContent = `${n1} + ${n2} = ${soma}`
 }
+
+function subtracao() {
+    // Números
+    let n1 = parseFloat(document.getElementById("numero1").value);
+    let n2 = parseFloat(document.getElementById("numero2").value);
+    // Operação
+    let subtracao = n1 - n2;
+    // Resultado
+    resultado.textContent = `${n1} - ${n2} = ${subtracao}`
+  }
+
+  function multiplicacao() {
+    // Números
+    let n1 = parseFloat(document.getElementById("numero1").value);
+    let n2 = parseFloat(document.getElementById("numero2").value);
+    // Operação
+    let multiplicacao = n1 * n2;
+    // Resultado
+    resultado.textContent = `${n1} x ${n2} = ${multiplicacao}`
+  }
+
+  function divisao() {
+    // Números
+    let n1 = parseFloat(document.getElementById("numero1").value);
+    let n2 = parseFloat(document.getElementById("numero2").value);
+    // Operação
+    let divisao = n1 / n2;
+    // Resultado
+    resultado.textContent = `${n1} ÷ ${n2} = ${divisao}`
+  }
